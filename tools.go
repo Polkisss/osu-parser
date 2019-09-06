cc1453797e8e99f7d31d5857cd3d94b50d0a0baa
Polkisss <createin2016lig@gmail.com>
2019-09-06 18:36:10 +0300

    added tokenizer and string2int2bool

--- tools.go
+++ tools.go
@@ -1,5 +1,10 @@
 package pcircle
 
+import (
+	"errors"
+	"strings"
+)
+
 func bool2int(b bool) (i int) {
 	if b {
 		i = 1
@@ -13,4 +18,19 @@ func bool2int2string(b bool) (s string) {
 		s = "1"
 	}
 	return s
+}
+
+func string2int2bool(s string) (b bool, err error) {
+	switch s {
+	case "0":
+		return false, nil
+	case "1":
+		return true, nil
+	}
+	return false, errors.New("failed to convert string to int to bool: invalid string " + s)
+}
+
+func tokenize(str string) (head, data string) {
+	sep := strings.IndexRune(str, ':')
+	return strings.TrimSpace(str[:sep]), strings.TrimSpace(str[sep+1:])
 }
